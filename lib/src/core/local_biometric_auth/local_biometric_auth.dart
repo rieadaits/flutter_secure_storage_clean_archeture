0ad332ade9329d664a8450a5fd683cfe3862ef5c
rieadaits <rieadaits21@gmail.com>
2025-12-23 15:11:34 +0600

    implemented biometric login

--- lib/src/core/local_biometric_auth/local_biometric_auth.dart
+++ lib/src/core/local_biometric_auth/local_biometric_auth.dart
@@ -3,6 +3,8 @@ import 'dart:developer';
 import 'package:flutter/services.dart';
 import 'package:local_auth/local_auth.dart';
 
+import 'package:local_auth_darwin/local_auth_darwin.dart';
+
 class LocalAuth {
   final _localAuthentication = LocalAuthentication();
 
@@ -20,8 +22,16 @@ class LocalAuth {
       return await _localAuthentication.authenticate(
         localizedReason: 'Scan your fingerprint to authenticate',
         biometricOnly: true,
+        authMessages: <AuthMessages>[
+      AndroidAuthMessages(
+          signInTitle: 'Oops! Biometric authentication required!',
+          cancelButton: 'No thanks',
+        ),
       );
     } on PlatformException catch (e) {
+      log(e.toString());
+      return false;
+    } catch (e) {
       log(e.toString());
       return false;
     }
